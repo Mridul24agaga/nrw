deec1cf4874c259ce947ac00072fc44288599cc3
Mridul24agaga <mridulthareja8@gmail.com>
2025-03-11 19:07:13 +0530

    ASG

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,5 @@
-import { cookies } from "next/headers"
 import LoginForm from "./login-form"
 
-export default async function LoginPage() {
-  const cookieStore = await cookies()
-  const theme = cookieStore.get("theme")
-
-  return <LoginForm  />
+export default function LoginPage() {
+  return <LoginForm />
 }
-
